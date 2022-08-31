b2a94fc3c537d4e105f6ab10bca6fe51646510f8
hazadus <hazadus7@gmail.com>
2022-08-31 21:37:55 +0300

    Added emojify.py

---
+++ emojify.py
@@ -0,0 +1,12 @@
+# Python Emoji
+#
+# pip3 install emoji
+#
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+import emoji
+
+
+re = emoji.emojize('Yoo Python Coder :wave:')
+print(re)  # 👋
+re = emoji.emojize('Hi Python Coder :grinning:')
+print(re)  # 😀
